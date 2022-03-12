b807d1df7d408e05b777c510369897e10bdd9a28
sanjeevkumar1kiwitech <sanjeev.kumar1@kiwitech.com>
2022-03-12 22:18:44 +0530

    add new activity

--- app/src/main/java/com/example/movieapp/activity/AddNewActivity.kt
+++ app/src/main/java/com/example/movieapp/activity/AddNewActivity.kt
@@ -2,6 +2,7 @@ package com.example.movieapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieapp.R
 
 class AddNewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
